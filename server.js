cfb6beea4e03463a4faecafc28b7051fbc6d5b20
Elizabeth Quispe <ellienq@outlook.com>
2024-01-18 23:09:04 -0500

    Comment the initializeMap and findAddress function temporarily.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
+//const https = require('https'); 
+//const fs = require('fs'); 
 const path = require('path');
 const dotenv = require('dotenv');
 
