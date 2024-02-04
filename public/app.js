c7df2d8ceb7d6af3673d410d674a9f5559147e6f
Elizabeth Quispe <ellienq@outlook.com>
2024-02-04 13:40:14 -0500

    Add an input field, select tag, and delete button when the user clicks on 'Add Subtask'.

--- public/app.js
+++ public/app.js
@@ -752,6 +752,80 @@ function findAddress(map, marker){
 initializeMap()
 */
 
+function removeSubtaskFromForm(subtasks){
+    const inputContainerArr = Array.from(subtasks.querySelectorAll('div.input-container'))
+    const removeSubtaskBtnArr = Array.from(subtasks.querySelectorAll('.remove-subtask-btn'))
+    console.log(subtasks) //main container
+    console.log(removeSubtaskBtnArr) //btn array
+    console.log(inputContainerArr.length) //one input container
+    
+    removeSubtaskBtnArr.map((function(btn, i){
+        btn.addEventListener('click', ()=>{
+            console.log('clicked', i)
+
+            //console.log(inputContainerArr)
+            inputContainerArr.splice(i, 1)
+
+            //console.log(inputContainerArr) //changes
+            //console.log(subtasks) //doesn't change
+            //subtasks.innerHTML = inputContainerArr
+            subtasks.removeChild(subtasks.childNodes[i]) //not working
+
+
+            /*categories.splice(i, 1)
+            localStorage.setItem('MyCategoryList', JSON.stringify(categories))
+            
+            displayCreatedCategories() */
+        })
+    }))
+}
+
+
+function addInputFieldSubtask(){
+    const addSubtask = document.querySelector('.subtask-label') //from html
+    const subtasks = document.getElementById('subtasks') //from html
+    addSubtask.addEventListener('click', ()=>{
+
+        displaySubtasks(subtasks)
+    })
+    
+}
+addInputFieldSubtask()
+
+
+function displaySubtasks(subtasks){
+
+    subtasks.innerHTML += `<div class="input-container">
+    <i class="fa-solid fa-circle-minus remove-subtask-btn"></i>
+   <input type="text" class="input-subtask" placeholder="new">
+   <div class="custom-select">
+    <select>
+        <option value="Set Priority">Set Priority</option>
+        <option value="High Priority">Today</option>
+        <option value="Medium Priority">Very Soon</option>
+        <option value="Low Priority">Next Week</option>
+    </select>
+   </div>
+   </div>
+   `
+   removeSubtaskFromForm(subtasks)
+}
+
+/*function displayPreviewOfTodoList(id, title, type, date, time, category, color){
+    let timeDisplay = ''
+    if(time === 'NaN: PM' || !time){
+        timeDisplay  
+    } else {
+        timeDisplay = `| ${time}`
+    }
+    
+    todoList.innerHTML += ` 
+                        <li class="list-item" id="${id}">
+                        <label class="container"> 
+                        <input type="checkbox" id="${id}" class="checkbox"><span class="checkmark"></span></label> ${title} <i class="fa-solid fa-chevron-right list-arrow"></i>
+                        </li><p class="list-details">${date} ${timeDisplay} | ${type} | ${category}<span class="color-box" style="background-color:${color};"></span></p>` */
+
+
 ////////////////// FORM SUBMISSION FOR NEW EVENT OR TASK ////////////////
 const mapID = document.getElementById('map')
 let formDataArr = [];
